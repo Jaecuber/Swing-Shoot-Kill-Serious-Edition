54139055b41eb831d5c8187836e5988b3233e87a
Jaecuber <pokemon.jaelas@gmail.com>
2026-06-21 16:12:40 -0700

    Play button func

--- core/src/main/java/com/github/Jaecuber/ui/model/MenuViewModel.java
+++ core/src/main/java/com/github/Jaecuber/ui/model/MenuViewModel.java
@@ -1,11 +1,17 @@
 package com.github.Jaecuber.ui.model;
 
 import com.github.Jaecuber.swingShootKill.Launcher;
+import com.github.Jaecuber.swingShootKill.asset.MapAsset;
+import com.github.Jaecuber.swingShootKill.screens.GameScreen;
 
 public class MenuViewModel extends ViewModel{
 
     public MenuViewModel(Launcher launcher) {
         super(launcher);
+    }
+
+    public void startGame(){
+        launcher.setScreen(new GameScreen(launcher, MapAsset.MAIN_MAP));
     }
 
 }
