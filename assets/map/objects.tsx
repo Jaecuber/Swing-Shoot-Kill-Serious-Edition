675b615b95c0430c2b9284f54a21845451d0d444
Meshp0tat <meishafenex@gmail.com>
2026-06-22 00:00:32 -0700

    Added Charlston

---
+++ assets/map/objects.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="objects" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <properties>
+   <property name="atlasAsset" value=""/>
+   <property name="health" type="float" value="0"/>
+   <property name="speed" type="float" value="0"/>
+   <property name="stamina" type="float" value="0"/>
+  </properties>
+  <image source="../../assets_raw/objects/charIcon.png" width="16" height="16"/>
+ </tile>
+</tileset>
